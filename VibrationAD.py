6b181123aad95a7429da6d5471200c102f63a7d5
kimjitae <wlxo0401@gmail.com>
2020-08-26 21:47:55 +0900

    1

--- VibrationAD.py
+++ VibrationAD.py
@@ -1,5 +1,12 @@
-
+import os
 
 class Vibration_Anomaly_Detection:
 
-    
+    def __init__(self):
+        # 데이터가 저장될 폴더 이름? 
+        self.folder_name = "C_33_200103_1405_D"
+        self.analysis_directory_path = "C:/Users/wlxo0/Desktop/asfs/" + self.folder_name + "_A/"
+
+    def folder_create(self):
+        if not(os.path.isdir(self.analysis_diretory_path)):
+            os.makedirs(os.path.join(self.analysis_diretory_path))
