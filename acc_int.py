aa52b3883cd86cf60e7f5a87b892815144d6ffa4
KimJitae <57979066+wlxo0401@users.noreply.github.com>
2020-08-27 21:35:38 +0900

    Update acc_int.py

--- acc_int.py
+++ acc_int.py
@@ -1,29 +1,3 @@
-'''
-function rtn=acc_int(data,fs)
-    df=fs/length(data);
-    data_fft=fft(data)/length(data);
-    data_fft_mag=(data_fft.*conj(data_fft)).^0.5;
-    
-    
-    faxis(:,1)=0:df:((fs-1/length(data)));
-    
-    data_fft_mag=data_fft_mag(1:(ceil(length(data)/2)))*2;
-    faxis=faxis(1:(ceil(length(data)/2)));
-    
-    rtn=zeros(floor(fs/2),1);
-    for n1=5:floor(fs/2-1)
-        tmp=0;
-        for n2=ceil((n1-0.5)/df+1):ceil((n1+0.5)/df)
-            tmp=tmp+(data_fft_mag(n2)/sqrt(2)/(2*pi*faxis(n2)))^2;
-        end
-        rtn(n1,1)=sqrt(tmp);
-    end
-        figure;
-        plot(rtn);
-        xlabel('Frequency(Hz)');
-        ylabel('v_{RMS}');
-end
-'''
 import numpy as np
 import math
 import matplotlib as plt
@@ -45,4 +19,3 @@ def acc_int(data, fs):
             tmp = tmp + np.power((data_fft_mag[n2]/math.sqrt(2) / (2*math.pi*faxis[n2])), 2)
         rtn[n1, 0] = math.sqrt(tmp)
     return rtn
-        
